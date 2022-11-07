67546d4069b6c80c27c72984851ce2a50832cfa3
Anna <chistozvonovaanna@gmail.com>
2022-11-07 22:45:12 +0300

    Registration with bootstrap

--- animation_studio_app/__init__.py
+++ animation_studio_app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
 app.secret_key = 'i want to sleep'
@@ -8,5 +10,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:admin@localhost:3306/anima
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 manager = LoginManager(app)
+Bootstrap(app)
 # db.create_all()
 
