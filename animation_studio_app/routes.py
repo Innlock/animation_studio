479570d6f59da6e0866bd88ea4b458f08c9c5b79
Anna <chistozvonovaanna@gmail.com>
2022-11-08 20:47:41 +0300

    Procedure+function added on main_page

--- animation_studio_app/routes.py
+++ animation_studio_app/routes.py
@@ -3,24 +3,27 @@ from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from animation_studio_app import db, app, manager
+from database.db_connection import get_db_connection
 from database.models import Projects, Employees, Files, Teams, ProjectsTeams, TeamsEmployees
 
 
-# @app.route('/')
-# def show_main_page():
-#     con = get_db_connection()
-#     with con.cursor() as cursor:
-#         cursor.execute("call get_projects_and_supervisors")
-#         rows = cursor.fetchall()
-#     con.close()
-#     return render_template('projects.html', projects=rows)
+@app.route('/')
+def show_main_page():
+    user_id = get_current_id()
+    # тут есть и процедура, и функция
+    con = get_db_connection()
+    with con.cursor() as cursor:
+        cursor.execute("call get_projects_and_supervisors")
+        projects = cursor.fetchall()
+        for project in projects:
+            cursor.execute(f"select count_people_in_project({project['id_project']}) as amount")
+            amount_of_people = cursor.fetchone()
+            project["amount"] = amount_of_people["amount"]
+    print(projects)
+    con.close()
+    user_logged = user_id is not None
+    return render_template('projects.html', projects=projects, user_logged=user_logged)
 
-# @manager.user_loader
-# def load_user(user_id):
-#     try:
-#         return Employees.query.get(user_id)
-#     except Exception:
-#         return None
 
 def get_current_id():
     if current_user.is_authenticated:
@@ -29,30 +32,30 @@ def get_current_id():
     return None
 
 
-@app.route('/')
-def show_main_page():
-    user_id = get_current_id()
-
-    # # все команды человека
-    # teams = db.session.query(Teams, Employees, TeamsEmployees). \
-    #     join(TeamsEmployees, TeamsEmployees.id_team == Teams.id_team).\
-    #     join(Employees, Employees.id_employee == TeamsEmployees.id_employee
-    #     or Employees.id_employee == Teams.leader).\
-    #     filter(Employees.id_employee == 1).group_by(Teams.id_team, Employees.id_employee).all()
-    # print(teams)
-
-    # # файлы по проекту
-    # files = db.session.query(Files, Employees, Projects).\
-    #     join(Employees, Employees.id_employee == Files.id_employee).\
-    #     join(Projects, Projects.id_project == Files.id_project).\
-    #     filter(Projects.id_project == 1).all()
-    # print(files)
-
-    projects = db.session.query(Projects, Employees). \
-        join(Employees, Employees.id_employee == Projects.supervisor).all()
-
-    user_logged = user_id is not None
-    return render_template('projects.html', projects=projects, user_logged=user_logged)
+# @app.route('/')
+# def show_main_page():
+#     user_id = get_current_id()
+#
+#     # # все команды человека
+#     # teams = db.session.query(Teams, Employees, TeamsEmployees). \
+#     #     join(TeamsEmployees, TeamsEmployees.id_team == Teams.id_team).\
+#     #     join(Employees, Employees.id_employee == TeamsEmployees.id_employee
+#     #     or Employees.id_employee == Teams.leader).\
+#     #     filter(Employees.id_employee == 1).group_by(Teams.id_team, Employees.id_employee).all()
+#     # print(teams)
+#
+#     # # файлы по проекту
+#     # files = db.session.query(Files, Employees, Projects).\
+#     #     join(Employees, Employees.id_employee == Files.id_employee).\
+#     #     join(Projects, Projects.id_project == Files.id_project).\
+#     #     filter(Projects.id_project == 1).all()
+#     # print(files)
+#
+#     projects = db.session.query(Projects, Employees). \
+#         join(Employees, Employees.id_employee == Projects.supervisor).all()
+#
+#     user_logged = user_id is not None
+#     return render_template('projects.html', projects=projects, user_logged=user_logged)
 
 
 @app.route('/login', methods=['GET', 'POST'])
